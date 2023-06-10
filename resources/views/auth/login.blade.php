0ff9838330dd4e1ed0c9b898a9101882f457a8d8
NeilVm <neilerickson512@gmail.com>
2023-06-10 00:48:38 -0500

    cambios

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -66,7 +66,7 @@
                                         <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                         <label class="form-check-label" for="remember">
-                                            {{ __('Recuérdame') }}
+                                            {{ __('Recordar Datos') }}
                                         </label>
                                     </div>
                                 </div>
@@ -74,10 +74,10 @@
 
                             <div class="row mb-0">
                                 <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit" class="btn btn-success">
                                         {{ __('¡Acceder!') }}
                                     </button>
-
+                                        <br>
                                     @if (Route::has('password.request'))
                                         <a class="btn btn-link" href="{{ route('password.request') }}">
                                             {{ __('¿Olvidaste tu contraseña?') }}
